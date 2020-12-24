e81c28b5806cbfa3d964182704f486e120e51a3e
ColdeZhang <coldezhang@foxmail.com>
2020-12-24 16:53:33 +0800

    Update test1.py

--- Test_Ground/test1.py
+++ Test_Ground/test1.py
@@ -31,7 +31,7 @@ def get_consel_size_test():
     for j in range(terminal_width - 2):
         unused_line = unused_line + " "
     unused_line = unused_line + "|\n"
-    
+
     while num < 10:
             #生成内容
         content = ""
@@ -63,4 +63,39 @@ def get_consel_size_test():
         num = num + 1
         time.sleep(1)    
 
-get_consel_size_test()
+def get_terminal_size():
+    '获取页面尺寸'
+    terminal_width = os.get_terminal_size().columns
+    terminal_height = os.get_terminal_size().lines-2
+    return [terminal_width, terminal_height]
+
+def instance_page():
+    '生成页面'
+    width = get_terminal_size()[0]
+    height = get_terminal_size()[1]
+
+    #生成顶部边线
+    top_botton_line = ""
+    top_botton_line = top_botton_line + "+"
+    for i in range(width - 2):
+        top_botton_line = top_botton_line + "-"
+    top_botton_line = top_botton_line + "+\n"
+
+    #生成左右边线
+    unused_line = ""
+    unused_line = unused_line + "|"
+    for i in range(width - 2):
+        unused_line = unused_line + " "
+    unused_line = unused_line + "|\n"
+
+    #将顶部边线与左右边线合并
+    page = ""
+    page = page + top_botton_line
+    for i in range(int(height)-2):
+        page = page + unused_line
+    page = page + top_botton_line
+
+    return page
+
+os.system('clear')
+sys.stdout.write ("{0}".format(instance_page()))
