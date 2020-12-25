8e51389b3df6506a8e66798069b6542f6d9b1768
ColdeZhang <coldezhang@foxmail.com>
2020-12-25 10:00:09 +0800

    Update test1.py

--- Test_Ground/test1.py
+++ Test_Ground/test1.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import time
+import keyboard
 
 
 def refresh_page_test1():
@@ -13,7 +14,7 @@ def refresh_page_test1():
         time.sleep(1)
 
 def get_consel_size_test():
-    '获取窗口行列大小'
+    '[已废弃]获取窗口行列大小'
     num = 1
     terminal_width = os.get_terminal_size().columns
     terminal_height = os.get_terminal_size().lines-2
@@ -21,7 +22,7 @@ def get_consel_size_test():
     #上下窗口边线
     top_botton_line = ""
     top_botton_line = top_botton_line + "+"
-    for j in range(terminal_width - 2):
+    for _ in range(terminal_width - 2):
         top_botton_line = top_botton_line + "-"
     top_botton_line = top_botton_line + "+\n"
 
@@ -64,42 +65,57 @@ def get_consel_size_test():
         time.sleep(1)    
 
 def get_terminal_size():
-    '获取页面尺寸'
+    '[可用]获取页面尺寸'
     terminal_width = os.get_terminal_size().columns
     terminal_height = os.get_terminal_size().lines-2
     return [terminal_width, terminal_height]
 
 def instance_page():
-    '生成页面'
+    '[可用]生成页面'
     width = get_terminal_size()[0]
     height = get_terminal_size()[1]
 
     #生成顶部边线
     top_botton_line = ""
     top_botton_line = top_botton_line + "+"
-    for i in range(width - 2):
+    for _ in range(width - 2):
         top_botton_line = top_botton_line + "-"
     top_botton_line = top_botton_line + "+\n"
 
     #生成左右边线
     unused_line = ""
     unused_line = unused_line + "|"
-    for i in range(width - 2):
+    for _ in range(width - 2):
         unused_line = unused_line + " "
     unused_line = unused_line + "|\n"
 
     #将顶部边线与左右边线合并
     page = ""
     page = page + top_botton_line
-    for i in range(height-2):
+    for _ in range(height-2):
         page = page + unused_line
     page = page + top_botton_line
 
     return page
 
+def keyboard_detect():
+    '[BUG]键盘侦测测试'
+    if keyboard.is_pressed("left"):
+        print ("left is pressed")
+        time.sleep(0.2)
+
+
+        
+    
 while True:
-    os.system('clear')
-    sys.stdout.write ("{0}".format(instance_page()))
+    keyboard_detect()
+
+# os.system('clear')
+# sys.stdout.write ("{0}".format(instance_page()))
+# while True:
+#     if get_terminal_size() != get_terminal_size():
+#         os.system('clear')
+#         sys.stdout.write ("{0}".format(instance_page()))
 
 
 
